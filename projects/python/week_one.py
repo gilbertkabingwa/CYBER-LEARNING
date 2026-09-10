b9dab4008628cff67132218371c8f6cf7087ac41
gilbertkabingwa <kabingwa1998@gmail.com>
2026-09-10 22:04:39 +0300

    week1

---
+++ projects/python/week_one.py
@@ -0,0 +1,34 @@
+#  Use Print() # Dinosaur
+
+print("...........__")
+print("........../  _)")
+print("...*-^^^-/  /..")
+print("__/......../...")
+print("<__.|_|-|_|...")
+
+
+
+# Project: Personal Profile 
+
+person = {
+    "name": "Romion Kalle",
+    "age": 35,
+    "country": "Finland",
+    "Student?": "Yes",
+    "Hight": "180cm"
+}
+print(person)
+
+ # Other way
+
+name = str(input("What is your name?:"))
+age = input("Enter you age:")
+country = "Finland"
+student = input("Are you a student?")
+height = float(input("What is your height?"))
+
+print("My name is", name,"\n"
+    "My age is", age, "\n"
+    "I was born in", country, "\n",
+    student, ", I do study", "\n",
+    "My height is", height)
