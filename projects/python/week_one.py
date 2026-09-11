3712218cd41427335693cfe902f51dc650b4f2c1
gilbertkabingwa <kabingwa1998@gmail.com>
2026-09-11 08:56:38 +0300

    project

--- projects/python/week_one.py
+++ projects/python/week_one.py
@@ -32,3 +32,17 @@ print("My name is", name,"\n"
     "I was born in", country, "\n",
     student, ", I do study", "\n",
     "My height is", height)
+
+# Fixed way or RRO Way
+
+name = input("What is your name?: ")
+age = int(input("Enter your age: "))
+country = "Finland"
+student = input("Are you a student?: ")
+height = float(input("What is your height?: "))
+
+print("My name is", name)
+print("My age is", age)
+print("I was born in", country)
+print("Student:", student)
+print("My height is", height)
